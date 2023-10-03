1c1cf03ec62099bd4e9a681c2aa06edf6ae5c310
Harendra Prajapati <harendraprajapati72@gmail.com>
2023-10-03 23:44:10 +0530

    array

---
+++ array_first_program.dart
@@ -0,0 +1,11 @@
+
+void main() {
+  var name = ["harendra", "vishal", "karan", "abhishek", "mihir", "saksham"];
+  print(name);// print array items
+  print(name.length);//print array length
+  print(name[0]);//print 0th index element of array
+  print(name.length - 1); // print last index number of array
+  print(name[name.length - 1]);//print last element of array
+  print(name.reversed);// reverse the strint
+  
+}
